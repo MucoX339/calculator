dbbc745e5dcb367b5e631f56bd143add27a8b96c
MucoX339 <116312114+MucoX339@users.noreply.github.com>
2022-10-21 01:07:39 +0300

    hesapmakinesi.py
    başlangıç seviyesi hesap makinesi kodları python

--- hesap.py
+++ hesap.py
@@ -1,4 +1,5 @@
 import math
+import os
 hesap_menu = """
 [+]Toplama Yapmak için
 [-]Çıkarma Yapmak için
@@ -13,31 +14,40 @@ def topla(a,b):
     sonuc=0
     sonuc = a+b
     print("toplamın sonucu :",sonuc)
+    input()
 def cikarma(a,b):
     sonuc=0
     sonuc = a-b
     print("çıkarma sonucu :",sonuc)
+    input()
 def carpma(a,b):
     sonuc=1
     sonuc = a * b
     print("çarpma işlemi sonucu :",sonuc)
+    input()
 def bolme(a,b):
     sonuc=1
     try:
         print("bölme işleminin sonucu :",float(a/b))
+        input()
     except ZeroDivisionError:
         print("0' a bölüm tanımsızdır :")
+        input()
 def üsalma(a,b):
     sonuc=1
     sonuc = a**b
     print("üs alma işlemi sonucu :",sonuc)
+    input()
 def kökalma(a):
     print("Sayının Kökü : ",int(math.sqrt(a)))
+    input()
 def kalan_bulma(a,b):
     sonuc=a%b
     sonuc1=int(a/b)
     print(a,"'in ",b,"bölüm sonucu :","bölüm :",sonuc1," kalan : ",sonuc)
+    input()
 while True:
+    
     print(30*"==",hesap_menu)
     secim =input("Yapıcağınız işlemi menüden seçiniz")
     if secim=="+":
